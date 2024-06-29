42e31cd4a76da17d4f3e060602e406f6ba52201c
luismistic <luis.escalada@hotmail.com>
2024-06-29 04:09:17 -0300

    cros

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:4200', // Origen permitido
+    origin: ['https://casa-abierta.online', 'http://localhost:4200'], // Orígenes permitidos, incluye tu dominio de producción y local
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Habilita el uso de cookies
   });
-  await app.listen(3000);
+
+  await app.listen(3000); // Puerto en el que escucha tu aplicación NestJS
 }
 bootstrap();
