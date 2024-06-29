3fdef83f4cf2be64350327541e21452e78607a72
luismistic <luis.escalada@hotmail.com>
2024-06-29 15:07:36 -0300

    smpt1

---
+++ smtp-test.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'c1701995.ferozo.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'casaabierta@casa-abierta.online',
+    pass: 'Luis3175',
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('Server is ready to take our messages:', success);
+  }
+});
